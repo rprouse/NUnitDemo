2d5c82de9a8f858e41d3eb55af7f791e3f4f7599
Rob Prouse <rob@prouse.org>
2019-03-20 09:07:56 -0700

    Added parallel tests

---
+++ NUnit.Demo.Tests/ParallelTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace NUnit.Demo.Tests
+{
+    [Parallelizable(ParallelScope.All)]
+    public class ParallelTests
+    {
+        [TestCaseSource(nameof(ParallelTestData))]
+        public async Task ParallelTest(int id)
+        {
+            // Do long running work
+            await Task.Delay(1000);
+
+            TestContext.WriteLine($"Test id: {id}, thread id: {Thread.CurrentThread.ManagedThreadId}");
+        }
+
+        public static IEnumerable<int> ParallelTestData() =>
+            Enumerable.Range(0, 10);
+    }
+}
