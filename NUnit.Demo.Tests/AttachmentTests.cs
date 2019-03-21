076939e246cfb23fe1ea13b5506bd1ef33f0cf49
Rob Prouse <rob@prouse.org>
2019-03-21 09:37:22 -0700

    Added attachments

---
+++ NUnit.Demo.Tests/AttachmentTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using NUnit.Framework;
+
+namespace NUnit.Demo.Tests
+{
+    public class AttachmentTests
+    {
+        [Test]
+        public void AttachFileToTests()
+        {
+            var filename = CreateHelloWorld();
+
+            Assert.That(filename, Does.Exist);
+
+            TestContext.AddTestAttachment(filename, "Hello World file");
+        }
+
+        string CreateHelloWorld()
+        {
+            var filename = Path.GetTempFileName();
+            File.WriteAllText(filename, "Hello World!");
+            return filename;
+        }
+    }
+}
